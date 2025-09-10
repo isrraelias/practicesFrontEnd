4c510254151424cde2e020ab6641c8b2aa9b4c65
atemnPC <isrraelias2@gmail.com>
2025-09-09 21:24:23 -0500

    avance conceptos

--- repaso-angular/todoapp/src/app/pages/labs/labs.ts
+++ repaso-angular/todoapp/src/app/pages/labs/labs.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-labs',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './labs.html',
   styleUrl: './labs.css'
 })
 export class Labs {
   msg: string = 'hola mundo';
   tareas : Array<string> = ['tarea 1 - Ag', 'tarea 2 - Ag', 'tarea 3 - Ag'];
+  codigo: string = 
+  `<ul>
+    @for(tarea of tareas; track tarea){
+    <li>
+      {{tarea}}
+    </li>
+    }
+  </ul>`;
+  estado: boolean = true;
 }
