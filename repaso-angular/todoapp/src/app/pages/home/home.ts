c6884b0a426596781a68045a03472d3e5da829a8
atemnPC <isrraelias2@gmail.com>
2025-11-24 22:46:54 -0500

    practica con elemento signal

--- repaso-angular/todoapp/src/app/pages/home/home.ts
+++ repaso-angular/todoapp/src/app/pages/home/home.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
+  taskList = signal([
+    'Learn Angular'
+    ,'Buy a unicorn' 
+    , 'Make a dish']);
+
+  showTasks(Event: Event) {
+    return this.taskList();
+  }
+
+  changeList(event: Event){
+    const element = event.target as HTMLInputElement;
+    const newTask = element.value;
+    this.taskList.update((taskList) => {
+     return [...taskList,newTask]
+    });
+  }
+
+  deleteTask(index: number){
+    this.taskList.update((estadoAnt) => {
+      return estadoAnt.filter((i) => i !== estadoAnt[index]);
+    })
+  }
 
 }
+
