13e52f6791bb905b6eebe70f43f7d47dd8931330
atemnLaptop <isrraelias2@gmail.com>
2026-06-01 21:43:52 -0500

    get de mis favoritos

--- repaso-angular/todoapp/src/app/pages/api-michis/api-michis.ts
+++ repaso-angular/todoapp/src/app/pages/api-michis/api-michis.ts
@@ -8,10 +8,13 @@ import { Component, signal } from '@angular/core';
 })
 export class ApiMichis {
   gatosphotos = signal<string[]>([]);
-  url = 'https://api.thecatapi.com/v1/images/search?limit=5&api_key=live_s10xsc5yPl9ChpsEGzT7bmBthnnXtgOy4w6HR5aXkXmwM3pxwLchBmPGFr9vlkxS';
-  
+  gatosphotosFavorite = signal<string[]>([]);
+  url = 'https://api.thecatapi.com/v1/images/search?limit=10&api_key=live_s10xsc5yPl9ChpsEGzT7bmBthnnXtgOy4w6HR5aXkXmwM3pxwLchBmPGFr9vlkxS';
+  urlFavorite = 'https://api.thecatapi.com/v1/favourites?api_key=live_s10xsc5yPl9ChpsEGzT7bmBthnnXtgOy4w6HR5aXkXmwM3pxwLchBmPGFr9vlkxS';
+
   ngOnInit(){
     this.getRandomMichi();
+    this.getFavoriteMichi();
   }
 
   async getRandomMichi() {
@@ -19,10 +22,20 @@ export class ApiMichis {
     const data = await response.json();
     const urlsImages = data.map((data: any) => data.url);
     this.gatosphotos.set(urlsImages);
+    console.log('data norm', data);
   }
 
-  getFavoriteMichi() {
-    console.log('Obtener gatito');
+  async getFavoriteMichi() {
+    const response = await fetch(this.urlFavorite);
+    const data = await response.json();
+    const urlsImages = data.map((data: any) => data.image.url);
+    this.gatosphotosFavorite.set(urlsImages);
+    // console.log('data favo', data);
   }
+
+  deleteFavoriteMichi() {
+    console.log('Eliminar gatito'); 
+  }
+
 
 }
