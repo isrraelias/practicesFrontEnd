fdf82868dc98c44e450ec6baf308ca224b610b74
Elias Manjarrez <isrraelias2@gmail.com>
2026-06-02 17:01:24 -0500

    metodo delete

--- repaso-angular/todoapp/src/app/pages/api-michis/api-michis.ts
+++ repaso-angular/todoapp/src/app/pages/api-michis/api-michis.ts
@@ -1,5 +1,11 @@
 import { Component, signal } from '@angular/core';
 
+//definimos una interfase para facilitar el manejo de los datos que recibimos de la api
+interface Michi {
+  id: string;
+  url: string;
+}
+
 @Component({
   selector: 'app-api-michis',
   imports: [],
@@ -7,15 +13,15 @@ import { Component, signal } from '@angular/core';
   styleUrl: './api-michis.css'
 })
 export class ApiMichis {
-  gatosphotos = signal<string[]>([]);
-  favoriteGatosPhotos = signal<string[]>([]);
+  gatosphotos = signal<Michi[]>([]);
+  favoriteGatosPhotos = signal<Michi[]>([]);
   msgError = '';
   msgErrorLoadFavorite = '';
   msgErrorSaveFavorite = '';
 
   url = 'https://api.thecatapi.com/v1/images/search';
-  limitApi = 'limit=10';
-  API_KEY = 'api_key=live_s10xsc5yPl9ChpsEGzT7bmBthnnXtgOy4w6HR5aXkXmwM3pxwLchBmPGFr9vlkxS';
+  limitApi = 'limit=8';
+  API_KEY = 'live_s10xsc5yPl9ChpsEGzT7bmBthnnXtgOy4w6HR5aXkXmwM3pxwLchBmPGFr9vlkxS';
   urlFav = 'https://api.thecatapi.com/v1/favourites';
 
   ngOnInit(){
@@ -26,9 +32,14 @@ export class ApiMichis {
   //trae gatos aleatorios de la api
   async getRandomMichi() {
     try {
-      const response = await fetch(this.url + '?' + this.limitApi + '&' + this.API_KEY);
+      const response = await fetch(this.url + '?' + this.limitApi + '&' + 'api_key=' +this.API_KEY);
       const data = await response.json();
-      const urlsImages = data.map((data: any) => [data.url, data.id]);
+
+      const urlsImages: Michi[] = data.map((data: any) => ({
+        id: data.id,
+        url: data.url
+      }));
+
       this.gatosphotos.set(urlsImages);
       console.log(data);
     } catch (error) {
@@ -46,11 +57,12 @@ export class ApiMichis {
           'x-api-key': this.API_KEY
         },
         body: JSON.stringify({
-          'image_id': 'trgTS8szc'
+          'image_id': id
         })
       });
       const data = await rest.json();
       console.log(data); 
+      this.getFavoriteMichi();
     } catch (error) {
       this.msgErrorSaveFavorite = 'Error al guardar gatito favorito 😿 - '  + error;
     }
@@ -59,12 +71,32 @@ export class ApiMichis {
   //trae los gatos favoritos del usuario
   async getFavoriteMichi() {
     try {
-      const response = await fetch(this.urlFav + '?' + this.API_KEY);
+      const response = await fetch(this.urlFav + '?' + 'api_key=' +this.API_KEY);
       const data = await response.json(); console.log('obtener fav',data);
-      const urlsImages = data.map((data: any) => data.image.url);
+
+      const urlsImages : Michi[] = data.map((data: any) => ({
+        id: data.id,
+        url: data.image.url
+      }));
+
       this.favoriteGatosPhotos.set(urlsImages); console.log(urlsImages);
     } catch (error) {
       this.msgErrorLoadFavorite = 'Error al obtener gatitos favoritos 😿 - '  + error;
+    }
+  }
+
+  async deleteFavoriteMichi(id: string) {
+    try{
+      const rest = await fetch(`${this.urlFav}/${id}`,{
+        method: 'DELETE',
+        headers: {
+          'Content-type': 'aplication/json',
+          'x-api-key': this.API_KEY
+        }
+      });
+      this.getFavoriteMichi();
+    } catch (error) {
+
     }
   }
 
