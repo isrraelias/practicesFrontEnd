ee992d15b047f88398095b5cd6455a9f767d2e52
atemnLaptop <isrraelias2@gmail.com>
2026-06-15 14:10:24 -0500

    proyecto tienda

---
+++ store-practiceTW/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { List } from './domains/products/pages/list/list'
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: List
+    }
+];
